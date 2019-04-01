41d412cabfddac68572dd73b031755d4c912bfca
ashk0821 <ashk0821@outlook.com>
2019-03-31 20:43:45 -0400

    hi

--- src/MazeSolver/MazeGenerator.java
+++ src/MazeSolver/MazeGenerator.java
@@ -1,4 +1,20 @@
 package MazeSolver;
 
+import java.awt.*;
+
 public class MazeGenerator {
+    int maze [][];
+    int rows = 41;
+    int columns = 41;
+    boolean mazeExists = false;
+
+    public void generateMaze() {
+        if (maze == null) {
+            maze = new int[rows][columns];
+        }
+
+        int
+
+    }
+
 }
