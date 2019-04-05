533fa3491d4bca45dc5f47954e0dca4ccb75277d
ashk0821 <ashk0821@outlook.com>
2019-04-05 10:13:39 -0400

    hi

--- src/MazeSolver/MazeGenerator.java
+++ src/MazeSolver/MazeGenerator.java
@@ -1,81 +1,201 @@
 package MazeSolver;
 
-import java.awt.*;
+import java.io.*;
+import java.util.*;
 
 public class MazeGenerator {
-    public int maze [][];
-    int rows = 41;
-    int columns = 41;
-    boolean mazeExists = false;
+    public final int uncheckedWallCode = 0;
+    public final int checkedWallCode = 1;
+    public Node [][] maze;
+    private int size;
+    private int [] startingPoint;
 
-    public static final int backgroundInt = 0;
-    public static final int wallInt = 1;
-    public static final int pathInt = 2;
-    public static final int emptyInt = 3;
-    public static final int visitedInt = 2;
+    public MazeGenerator (int size, int[] startingPoint){
+        this.startingPoint = startingPoint;
+        this.size = size;
 
-    public void generateMaze() {
-        if (maze == null) {
-            maze = new int[rows][columns];
-        }
-
-        /* Number of rooms and walls defaulted to 0 */
-        int emptyRoomCount = 0, wallCount = 0;
-
-        /* Position of the walls between the rooms */
-        int [] wallRows = new int [(rows * columns) / 2], wallColumns = new int [(rows * columns) / 2];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns ; j++) {
-                maze[i][j] = wallInt;
+        maze = new Node[this.size][this.size];
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                maze[i][j] = new Node(i, j, maze);
+                maze[i][j].setWallCode(uncheckedWallCode);
             }
         }
-
-        for (int i = 1; i < rows - 1; i += 2) {
-            for (int j = 1; j < columns -1 ; j++) {
-                emptyRoomCount++;
-                maze[i][j] = -(emptyRoomCount);
-
-                if (i < rows - 2){
-                    wallRows[wallCount] = i + 1;
-                    wallColumns[wallCount] = j;
-                    wallCount++;
-                }
-                if (j < columns - 2){
-                    wallRows[wallCount] = i;
-                    wallColumns[wallCount] = j + 1;
-                    wallCount++;
-                }
-            }
-        }
-
-        mazeExists = true;
-        int randomWall = 0;
-        for (int i = wallCount - 1; i > 0; i--) {
-            /* https://dzone.com/articles/random-number-generation-in-java */
-            randomWall = (int) (Math.random() * i);
-            removeWall(wallRows[randomWall], wallColumns[randomWall]);
-            wallRows[randomWall] = wallRows[i];
-            wallColumns[randomWall] = wallColumns[i];
-        }
-
-        for (int i = 1; i < rows - 1 ; i++) {
-            for (int j = 1; j < columns - 1; j++) {
-                if (maze[i][j] < 0){
-                    maze[i][j] = emptyInt;
-                }
-            }
-        }
-
     }
 
-    public void removeWall (int row, int column){
-        if (row % 2 == 1 && maze[row][column - 1] != maze[row][column + 1]){
-            maze[row][column] = maze[row][column];
+    public MazeGenerator (Node[][] maze){
+        this.maze = maze;
+        this.size = maze.length;
+        startingPoint = startPoint();
+    }
+
+    public int[] startPoint(){
+        int[] startingPoint = new int[2];
+
+        for (int j = 0; j < maze[0].length; j++) {
+            if (!maze[0][j].top_wall) {
+                startingPoint[0] = maze[0][j].horizontalNode;
+                startingPoint[1] = maze[0][j].verticalNode;
+            }
         }
-        else if (row % 2 == 0 && maze[row - 1][column] != maze[row + 1][column]){
-            maze[row][column] = maze[row + 1][column];
+
+        return startingPoint;
+    }
+
+    public boolean generate() throws IOException{
+        maze[startingPoint[0]][startingPoint[1]].setWallCode(checkedWallCode);
+        maze[startingPoint[0]][startingPoint[1]].top_wall = false;
+
+        ArrayList<Node> list = new ArrayList<>();
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                if (maze[i][j].getWallCode() == checkedWallCode) {
+                    ArrayList<Node> temp = maze[i][j].uncheckedNodes();
+                    for (Node node : temp) {
+                        list.add(node);
+                    }
+                }
+            }
+        }
+
+        if (list.size() == 0) {
+            Random r = new Random();
+            int rand_int = r.nextInt(size);
+            maze[maze.length - 1][rand_int].bot_wall = false;
+            write_to_file();
+            return true;
+        }
+
+        Random rand = new Random();
+        Node current_node = list.get(rand.nextInt(list.size()));
+        list = current_node.checkedNodes();
+        Random rand2 = new Random();
+        Node connection_node = list.get(rand2.nextInt(list.size()));
+
+        if (connection_node.verticalNode - 1 == current_node.verticalNode)
+            current_node.open_right();
+        else if (connection_node.verticalNode + 1 == current_node.verticalNode)
+            current_node.open_left();
+        else if (connection_node.horizontalNode - 1 == current_node.horizontalNode)
+            current_node.open_bot();
+        else if (connection_node.horizontalNode + 1 == current_node.horizontalNode)
+            current_node.open_top();
+
+        current_node.setWallCode(checkedWallCode);
+        return false;
+    }
+
+    public void write_to_file() throws IOException {
+        String write_to_maze = "";
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                if (maze[i][j].top_wall) {
+                    write_to_maze += ("---");
+                } else
+                    write_to_maze += ("   ");
+            }
+            write_to_maze += "\n";
+
+            for (int j = 0; j < maze[i].length; j++) {
+                if (maze[i][j].left_wall) {
+                    if (j == maze[i].length - 1)
+                        write_to_maze += "|"+maze[i][j].getWallCode()+ "|";
+                    else
+                        write_to_maze += ("|"+maze[i][j].getWallCode()+ " ");
+                }
+                else {
+                    if (j == maze[i].length - 1)
+                        write_to_maze += " "+maze[i][j].getWallCode()+ "|";
+                    else
+                        write_to_maze += (" "+maze[i][j].getWallCode()+ " ");
+                }
+            }
+            write_to_maze += "\n";
+
+        }
+
+        for (int j = 0; j < maze[maze.length - 1].length; j++) {
+
+            if (maze[maze.length - 1][j].bot_wall)
+                write_to_maze += "---";
+            else
+                write_to_maze += "   ";
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Maze.txt"));
+        writer.write(write_to_maze);
+
+        writer.close();
+    }
+
+    public void write_to_array_file(String file_name){
+        try {
+            FileOutputStream fos = new FileOutputStream(file_name);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(maze);
+            oos.close();
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
         }
     }
+    public void write_to_file(Node[][] temp_maze) throws IOException {
+        String write_to_maze = "";
+        for (int i = 0; i < temp_maze.length; i++) {
+            for (int j = 0; j < temp_maze[i].length; j++) {
+                if (temp_maze[i][j].top_wall) {
+                    write_to_maze += ("---");
+                } else
+                    write_to_maze += ("   ");
+            }
+            write_to_maze += "\n";
+
+            for (int j = 0; j < temp_maze[i].length; j++) {
+                if (temp_maze[i][j].left_wall) {
+                    if (j == temp_maze[i].length - 1)
+                        write_to_maze += "|"+maze[i][j].getWallCode()+ "|";
+                    else
+                        write_to_maze += ("|"+maze[i][j].getWallCode()+ " ");
+                } else {
+                    if (j == temp_maze[i].length - 1)
+                        write_to_maze += " "+maze[i][j].getWallCode()+ "|";
+                    else
+                        write_to_maze += (" "+maze[i][j].getWallCode()+ " ");
+                }
+            }
+            write_to_maze += "\n";
+
+        }
+
+        for (int j = 0; j < temp_maze[temp_maze.length - 1].length; j++) {
+
+            if (temp_maze[temp_maze.length - 1][j].bot_wall)
+                write_to_maze += "---";
+            else
+                write_to_maze += "   ";
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Maze.txt"));
+        writer.write(write_to_maze);
+
+        writer.close();
+    }
+
+    public int[] end_point(){
+
+        int[] end = new int[2];
+        for (int i = 0; i < maze.length; i++) {
+            if(!(maze[maze.length-1][i].bot_wall)){
+                end[0] = i;
+                end[1] = maze.length-1;
+            }
+        }
+
+        return end;
+    }
+
+
+
 
 }
+
--- src/MazeSolver/MazeSolver.java
+++ src/MazeSolver/MazeSolver.java
@@ -1,20 +1,49 @@
 package MazeSolver;
 
-public class MazeSolver extends MazeGenerator {
-    public boolean solveMaze(int row, int column){
-        if (maze[row][column] == emptyInt){
-            maze[row][column] = pathInt;
-            if ((row == rows - 2) && (column == columns - 2)){
-                return true;
-            }
-            if (solveMaze(row - 1, column) || solveMaze(row, column- 1) || solveMaze(row + 1, column)
-                    || solveMaze(row, column + 1)){
-                return true;
-            }
-            maze[row][column] = visitedInt;
-        }
-        return false;
+import java.util.ArrayList;
+import java.util.Random;
 
+public class MazeSolver {
+    private boolean isFinished = false;
+    public final int checked_value = 2;
+    public final int wrong_way_value = 3;
+
+    public void solve(Node[][] maze) {
+        ArrayList<Node> paths = new ArrayList<Node>();
+        MazeGenerator Maze = new MazeGenerator(maze);
+        int[] start = Maze.startPoint();
+        int[] end = Maze.end_point();
+
+        paths = maze[start[0]][start[1]].path_options();
+        Random rand_int = new Random();
+        Node next = paths.get(rand_int.nextInt(paths.size()));
+        maze[start[0]][start[1]].setWallCode(checked_value);
+        maze[next.horizontalNode][next.verticalNode].setWallCode(checked_value);
+
+        while(!isFinished) {
+            paths = next.path_options();
+            if(next.verticalNode == end[0]&& next.horizontalNode == end[1]) {
+                isFinished = true;
+                next.setWallCode(checked_value);
+            }
+            else if(paths.size() == 0) {
+                next.setWallCode(wrong_way_value);
+                next = next.back_tracing();
+            }
+            else {
+                maze[next.horizontalNode][next.verticalNode].setWallCode(checked_value);
+                rand_int = new Random();
+                next = paths.get(rand_int.nextInt(paths.size()));
+            }
+        }
+
+        MazeAnimator.solver_called = true;
+        try {
+            Maze.write_to_file();
+        }
+        catch (Exception ex){
+            System.out.println("Error");
+        }
     }
 
 }
